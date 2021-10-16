0b181bea48d02423fd82588272679612014335f8
David Gomez-Urquiza <david.gurquiza@gmail.com>
2021-10-16 11:26:37 -0500

    Change image building behavior
    - Only generate squared images
    - Fix name length to 32 characters
    - Create an initial image of a given initial_size and scale it up to a output_size + applying a Gaussian filter
    - Update to image crate 0.23 and use default features to support other formats
    - Update other dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,23 @@ use rayon::prelude::*;
 use std::iter;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use image::imageops::FilterType;
 
 #[derive(StructOpt, Debug)]
 struct Options {
+    #[structopt(long = "initial-size", default_value = "2")]
+    initial_size: u32,
+
     #[structopt(short = "o", long = "output-dir", default_value = ".", parse(from_os_str))]
     output_directory: PathBuf,
 
-    #[structopt(long = "max-width", default_value = "1024")]
-    max_width: u32,
+    #[structopt(long = "output-size", default_value = "1024")]
+    output_size: u32,
 
-    #[structopt(long = "max-height", default_value = "1024")]
-    max_height: u32,
+    #[structopt(long = "output-format", default_value = "jpg")]
+    output_format: String,
 
-    #[structopt(name = "COUNT", default_value = "1")]
+    #[structopt(name = "count", default_value = "1")]
     count: u32,
 }
 
@@ -31,42 +35,36 @@ fn main() {
         let mut rng = rand::thread_rng();
         let mut path = options.output_directory.clone();
 
-        let filename = gen_filename(&mut rng, ".jpg");
+        let filename = gen_filename(&mut rng, options.output_format.as_str());
         println!("  [{}/{}] {}", i + 1, count, filename);
         path.push(filename);
 
         let buf = gen_image(&mut rng, &options);
 
-        if let Err(e) = buf.save(path) {
-            println!("error: {}", e);
-        }
+        buf.save(path).expect("Failed saving file")
     });
 }
 
 fn gen_filename(mut rng: impl RngCore, extension: &str) -> String {
-    let length = rng.gen_range(4, 128);
-    let mut name = String::with_capacity(length + extension.len());
+    let length = 32;
+    let mut name = String::with_capacity(length + 1 + extension.len());
 
     for c in iter::repeat(()).map(|_| rng.sample(Alphanumeric)).take(length) {
         name.push(c);
     }
 
+    name.push_str(".");
     name.push_str(extension);
 
     name
 }
 
 fn gen_image(mut rng: impl RngCore, options: &Options) -> ImageBuffer<Rgb<u8>, Vec<u8>>  {
-    let width = rng.gen_range(32, options.max_width);
-    let height = rng.gen_range(32, options.max_height);
-
-    let mut buf = image::ImageBuffer::new(width, height);
+    let mut buf = image::ImageBuffer::new(options.initial_size, options.initial_size);
 
     for (_, _, pixel) in buf.enumerate_pixels_mut() {
-        *pixel = image::Rgb {
-            data: [rng.gen_range(0, 255), rng.gen_range(0, 255), rng.gen_range(0, 255)],
-        };
+        *pixel = image::Rgb([rng.gen_range(0, 255), rng.gen_range(0, 255), rng.gen_range(0, 255)],);
     }
 
-    buf
+    image::imageops::resize(&buf, options.output_size, options.output_size, FilterType::Gaussian)
 }
